faf692875d440d0f79583e0b599e19c759c3a09b
Nacho418 <110699692+Nacho418@users.noreply.github.com>
2022-08-19 15:14:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 function comprar() {
     sumaTot = 0
     precioTotal = 0
+    productos = 0
+    vendido = new Venta()
+    peso = 0
     newItem()
+    console.log("Venta N° " + vendido.numVenta)
+    console.log(vendido)
+    calcAcum(cantVentas)
+    console.log("Total en ventas acumuladas = $" + acum)
 }
 function newItem() {
     let type = parseInt(prompt("Elija tipo de chapa"))
@@ -38,7 +45,9 @@ function itemPrice(valorKg) {
     }
     alert("Usted ingresó " + cantidadKg + "kg")
     sumaPar = + cantidadKg * valorKg
+    peso += cantidadKg
     alert("$" + sumaPar + " añadido al carrito")
+    productos++
     continuar()
     return sumaPar
 }
@@ -50,7 +59,11 @@ function continuar() {
     }
     else if (respuesta == "N") {
         alert("Su total es de $" + sumaTot)
+        numVenta++
         consultar(sumaTot)
+        vendido.numVenta = numVenta
+        vendido.cantProd = productos
+        vendido.pesoTot = peso
     }
     else {
         continuar()
@@ -62,22 +75,62 @@ function consultar(sumaTot) {
     if (cuotas == "Y") {
         let cantCuotas = 1
         while (cantCuotas <= 1 || cantCuotas > 18 || cantCuotas == null || isNaN(cantCuotas))
-            cantCuotas = prompt("Ingrese cantidad de cuotas (2 a 18 - 5% mensual)")
+            cantCuotas = parseInt(prompt("Ingrese cantidad de cuotas (2 a 18 - 5% mensual)"))
         for (let i = 2; i <= cantCuotas; i++) {
             precioTotal *= 1.05
         }
-        alert("Su total a abonar es de $" + Math.floor(precioTotal) + " en " + cantCuotas + " pagos.")
-        alert("Gracias por su compra")
+        cantVentas[numVenta - 1] = Math.floor(precioTotal)
+        vendido.enCuotas = true
+        vendido.pago = Math.floor(precioTotal)
+        vendido.cantCuotas = cantCuotas
+        ventas.push(vendido)
+        finalizarVenta(vendido)
+
     }
     else if (cuotas == "N") {
-        alert("Su total a abonar es de $" + Math.floor(precioTotal) + " en 1 pago")
-        alert("Gracias por su compra")
+        cantCuotas = 1
+        cantVentas[numVenta - 1] = Math.floor(precioTotal)
+        vendido.enCuotas = false
+        vendido.pago = Math.floor(precioTotal)
+        vendido.cantCuotas = 1
+        ventas.push(vendido)
+        finalizarVenta(vendido)
     }
     else {
         consultar(sumaTot)
     }
 }
+function finalizarVenta(venta) {
+    if (venta.cantCuotas == 1) {
+        alert("Su total a abonar es de $" + venta.pago + " en " + venta.cantCuotas + " pago.")
+    }
+    else {
+        alert("Su total a abonar es de $" + venta.pago + " en " + venta.cantCuotas + " pagos de $" + Math.floor(venta.pago / venta.cantCuotas) + ".")
+    }
+    alert("Gracias por su compra")
+}
+class Venta {
+    constructor(numVenta, cantProd, pago, enCuotas, cantCuotas, pesoTot) {
+        this.numVenta = numVenta;
+        this.cantProd = cantProd;
+        this.pago = pago;
+        this.enCuotas = enCuotas;
+        this.cantCuotas = cantCuotas;
+        this.pesoTot = pesoTot;
+    }
+}
+function calcAcum(cantVentas) {
+    for (let i = 0; i <= cantVentas.length - 1; i++) {
+        acum += cantVentas[i]
+    }
+    return acum
+}
+let acum = 0
+let ventas = []
+let cantVentas = []
+let productos = 0
 let sumaTot = 0
+let numVenta = 0
 alert("Los tipos de chapa se visualizan en el HTML")
 let boton = document.querySelector("#Comprar")
 boton.addEventListener("click", comprar)
